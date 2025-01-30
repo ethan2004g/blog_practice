3b23d592a6e3cff37c54711d3d8378de5ae0212b
Hunter Han <hunter.han@gmail.com>
2025-01-30 18:09:21 +0000

    Add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
 #  has_many :comments
 #add another comment. Yeah
 
+# not valid without first and last name
+    validates :first_name, :last_name, presence: true
 
 end
